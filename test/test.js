8db9082b957c5e4a20aaba972696f6605a93e43d
Sheethal <85963633+SheethalBangera@users.noreply.github.com>
2023-01-19 19:29:01 +0530

    Create test.js

---
+++ test/test.js
@@ -0,0 +1,24 @@
+{
+  "name": "simple-bank-system",
+  "version": "1.0.0",
+  "description": "",
+  "main": "app.js",
+  "scripts": {
+    "start": "node app.js",
+    "test": "mocha --recursive --exit"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "body-parser": "^1.19.0",
+    "dotenv": "^10.0.0",
+    "ejs": "^3.1.6",
+    "express": "^4.17.1",
+    "mongoose": "^5.13.2",
+    "nodemon": "^2.0.9"
+  },
+  "devDependencies": {
+    "mocha": "^10.2.0"
+  }
+}
