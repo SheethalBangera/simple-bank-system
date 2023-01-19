272602116e78fcf2993d466eebee7fb55328b795
Sheethal <85963633+SheethalBangera@users.noreply.github.com>
2023-01-19 12:59:38 +0530

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -9,7 +9,7 @@ const authHandler = require('./auth/authHandler')
 // Routes in use
 router.use('/bank', accountHandler)
 router.use('/auth', authHandler)
-router.use('/', (_, res) => res.status(200).json({msg: 'Bem vindo à API de Sistema bancário', author: 'mablds', docs: 'https://github.com/mablds/simple-bank-system'}))
+router.use('/', (_, res) => res.sendFile(__dirname + "/index.html"))
 router.use('*', (_, res) => res.status(404).json({msg: 'Não tem nada para você por aqui 👀'}))
 
 module.exports = router
