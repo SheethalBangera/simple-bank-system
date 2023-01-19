a9e183a0da0a95dad446ef64ac98c8e6826fe019
Sheethal <85963633+SheethalBangera@users.noreply.github.com>
2023-01-19 15:29:40 +0530

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -13,6 +13,7 @@ router.use('/', (_, res) => res.sendFile(__dirname + "/index.html"))
 router.use('/', (_, res) => res.sendFile(__dirname + "/logo.png"))
 router.use('/', (_, res) => res.sendFile(__dirname + "/banking.jpg"))
 router.use('/', (_, res) => res.sendFile(__dirname + "/style.css"))
+router.use('/', (_, res) => res.sendFile(__dirname + "/script.js"))
 router.use('*', (_, res) => res.status(404).json({msg: 'Não tem nada para você por aqui 👀'}))
 
 module.exports = router
