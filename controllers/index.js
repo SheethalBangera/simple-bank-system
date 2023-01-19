f2c893a71140ae69f19568d8c147f452c80e2f7f
Sheethal <85963633+SheethalBangera@users.noreply.github.com>
2023-01-19 14:10:26 +0530

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -9,7 +9,7 @@ const authHandler = require('./auth/authHandler')
 // Routes in use
 router.use('/bank', accountHandler)
 router.use('/auth', authHandler)
-router.use('/', (_, res) => res.sendFile("/root/simple-bank-system/controllers/index.html"))
+router.use('/', (_, res) => res.sendFile(__dirname + "/index.html"))
 router.use('*', (_, res) => res.status(404).json({msg: 'Não tem nada para você por aqui 👀'}))
 
 module.exports = router
